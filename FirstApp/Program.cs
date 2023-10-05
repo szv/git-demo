8bd11fd05f398134cbe0c6242b2b977935b19e3e
Sebastian Szvetecz <sebastian@szvetecz.at>
2023-10-05 15:26:52 +0200

    Removed write-lines

--- FirstApp/Program.cs
+++ FirstApp/Program.cs
@@ -1,7 +1,1 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
-Console.WriteLine("This is my new cool feature.");
-
-Console.WriteLine("This is my new second cool feature.");
-
-Console.WriteLine("This is my new third cool feature.");
+﻿Console.WriteLine("Hello, World!");
